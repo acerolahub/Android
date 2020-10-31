d194ec672e8026e837f7b9d80612c3a2348bf8f6
ralphsagbo <ralphsagbo2@gmail.com>
2020-10-31 19:42:05 +0100

    2-24: Exercise: Adding the Navigation Drawer

--- andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,25 +19,30 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout : DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
         // Find the navController from myNavHostFragment
         // Since we're using KTX, we can call this.findNavController
         val navController = this.findNavController(R.id.myNavHostFragment)
         // Link the navController to our ActionBar
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     // Ctrl-O pour afficher toutes les methodes qu'on peut override
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
